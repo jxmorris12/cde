local gradcache and fix gradient-sharing bug!

finetune.py
@@ -18,6 +18,9 @@ from trainer import CustomTrainer
 
 assert torch.cuda.device_count() > 0, "can't train without CUDA"
 
+# Helps with debugging.
+torch.autograd.set_detect_anomaly(True)
+
 # Allow W&B to start slowly.
 os.environ["WANDB__SERVICE_WAIT"] = "300"
 os.environ["_WANDB_STARTUP_DEBUG"] = "true"
@@ -89,7 +92,5 @@ trainer = CustomTrainer(
     eval_dataset=None,
     retrieval_datasets=retrieval_datasets,
 )
-trainer.evaluate_retrieval_datasets()
-
 trainer.train()
 

model.py
@@ -78,26 +78,31 @@ class Model(transformers.PreTrainedModel):
         
         batch_size = query_embedding.shape[0]
         input_query_embedding = query_embedding # save for biencoder
+
         query_embedding = self.query_projection(query_embedding)
         query_embedding = query_embedding.reshape((batch_size, self.n_sequence, self.hidden_size))
         assert query_embedding.shape == (batch_size, self.n_sequence, self.hidden_size)
 
-        document_embeddings = self.corpus_projection(document_embeddings)
         if self.share_hard_negatives:
-            document_embeddings = document_embeddings.repeat((batch_size, 1, 1))
+            document_embeddings = document_embeddings[None].repeat((batch_size, 1, 1))
         else:
-            inbatch_document_embeddings = document_embeddings[:batch_size, :]
-            inbatch_document_embeddings = (
-                inbatch_document_embeddings[:, None, :]
-                    .repeat((1, batch_size, 1))
+            pos_document_embeddings = document_embeddings[:batch_size, :]
+            pos_document_embeddings = (
+                pos_document_embeddings[None, :, :]
+                    .repeat((batch_size, 1, 1))
             )
             hn_document_embeddings = (
-                document_embeddings[batch_size:, :].reshape((batch_size, -1, self.hidden_size))
+                document_embeddings[batch_size:, :]
+                )
+            hn_document_embeddings = (
+                hn_document_embeddings.reshape((batch_size, -1, self.hidden_size))
             )
             document_embeddings = torch.cat((
-                inbatch_document_embeddings, hn_document_embeddings
+                pos_document_embeddings, hn_document_embeddings
             ), dim=1)
         
+        input_document_embeddings = document_embeddings # save for biencoder
+        document_embeddings = self.corpus_projection(document_embeddings)
         _, corpus_size, hidden_dim = document_embeddings.shape
         assert _ == batch_size
 
@@ -109,7 +114,7 @@ class Model(transformers.PreTrainedModel):
             )
             output_vectors = output.last_hidden_state[:, self.n_sequence:, :]
             query_output_vectors = output.last_hidden_state[:, :self.n_sequence, :].mean(dim=1)
-            scores = torch.bmm(output_vectors, query_output_vectors[:,:, None])
+            scores = torch.bmm(output_vectors, query_output_vectors[:, :, None])
             scores = scores.squeeze(dim=2)
         elif self.config.architecture == "query_independent":
             output = self.backbone(
@@ -135,7 +140,10 @@ class Model(transformers.PreTrainedModel):
             
             scores = torch.einsum('bd,bcd->bc', query_embedding, output_vectors)
         elif self.config.architecture == "biencoder":
-            scores = torch.einsum('bd,bcd->bc', input_query_embedding, document_embeddings)
+            # here we just throw away the projections
+            # input_query_embedding /= input_query_embedding.norm(p=2, dim=-1, keepdim=True)
+            # document_embeddings /= input_document_embeddings.norm(p=2, dim=-1, keepdim=True)
+            scores = torch.einsum('bd,bcd->bc', input_query_embedding, input_document_embeddings)
         else:
             raise ValueError(f"unknown architecture {self.config.architecture}")
 

run_args.py
@@ -139,6 +139,10 @@ class TrainingArguments(transformers.TrainingArguments):
         default="saves",
         metadata={"help": "Output directory for training saves"}
     )
+    use_gc: bool = field(
+        default=False,
+        metadata={"help": "whether to use GradCache"}
+    )
     num_train_epochs: float = field(
         default=100.0, 
         metadata={
@@ -193,7 +197,6 @@ class TrainingArguments(transformers.TrainingArguments):
             os.environ["WANDB_MODE"] = "disabled"
         ############################################################################
         num_workers = int(len(os.sched_getaffinity(0)) / torch.cuda.device_count())
-        num_workers = 0
         ############################################################################
         os.environ["RAYON_RS_NUM_CPUS"] = str(
             num_workers

trainer.py
@@ -4,12 +4,13 @@ import datasets
 import torch
 import transformers
 
-import grad_cache
 import wandb
 
+from gradcache import GradCache
 from dataset import BeirDataset
 from helpers import RerankHelper
 from model import Model
+
 
 def inputs_for_key(inputs: Dict[str, torch.Tensor], key: str):
     key += "_"
@@ -30,18 +31,29 @@ class CustomTrainer(transformers.Trainer):
             "query_input_ids", "query_attention_mask",
         ]
         print(f"initializing GradCache with chunk_size={self.args.max_batch_size_fits_in_memory}.")
-        self.gc = grad_cache.GradCache(
+        
+        self.use_gc = self.args.use_gc # whether to use gradcache
+        self.gc = None # lazily initialized during training
+    
+    def evaluate(*args, **kwargs) -> Dict[str, float]:
+        return {}
+
+    def create_optimizer(self, *args, **kwargs):
+        super().create_optimizer(*args, **kwargs)
+        self.gc = GradCache(
             models=[self.model.forward_embedder, self.model.forward_embedder],
+            optimizer=self.optimizer,
             chunk_sizes=self.args.max_batch_size_fits_in_memory,
             loss_fn=self._contrastive_loss, 
             fp16=self.args.fp16,
             scaler=self.scaler if self.args.fp16 else None,
+            backward_fn=self.accelerator.backward,
         )
     
-    def evaluate(*args, **kwargs) -> Dict[str, float]:
-        return {}
-    
     def training_step(self, model: torch.nn.Module, inputs: Dict[str, Union[torch.Tensor, Any]]) -> torch.Tensor:
+        if not self.use_gc:
+            return super().training_step(model=model, inputs=inputs)
+
         # Overriding from trainer so that we can disable backward()
         # in favor of the gradcache backward()
         model.train()
@@ -53,13 +65,11 @@ class CustomTrainer(transformers.Trainer):
         return loss.detach() / self.args.gradient_accumulation_steps
 
     def _contrastive_loss(self, e1: torch.Tensor, e2: torch.Tensor) -> torch.Tensor:
-        batch_size = len(e1)
-        corpus_size = len(e2)
-
         scores = self.model(
             query_embedding=e1,
             document_embeddings=e2,
         )
+        batch_size, corpus_size = scores.shape
         # This multiply-by-20 thing is used in BeIR and LaPRador:
         # "When calculating the loss, we apply a re-scaling trick
         # of multiplying the cosine similarity score by 20 for
@@ -120,7 +130,13 @@ class CustomTrainer(transformers.Trainer):
         }
         # print("query_inputs >>", {k: v.shape for k,v in query_inputs.items()})
         # print("all_document_inputs >>", {k: v.shape for k,v in all_document_inputs.items()})
-        return self.gc(query_inputs, all_document_inputs, no_sync_except_last=False)
+
+        if self.use_gc:
+            return self.gc(query_inputs, all_document_inputs, no_sync_except_last=False)
+        else:
+            e1 = self.model.forward_embedder(**query_inputs)
+            e2 = self.model.forward_embedder(**all_document_inputs)
+            return self._contrastive_loss(e1, e2)
     
     # Custom retrieval evalution code
     def _retrieval_evaluate(