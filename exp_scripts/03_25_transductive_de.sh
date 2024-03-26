784f3ea8f5afdd0a2c2c4c11fc1bb11a494305a6
Jack Morris <jxmorris12@gmail.com>
2024-03-26 20:04:11 +0000

    encoder-decoder exp; fix small bug with eval reranking in that setting

---
+++ exp_scripts/03_25_transductive_de.sh
@@ -0,0 +1,8 @@
+CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nproc_per_node 4 --rdzv-backend=c10d --rdzv-endpoint=localhost:0 --master_port 0 finetune.py --per_device_train_batch_size 224 --per_device_eval_batch_size 224 --use_wandb 1 --bf16 1 --dataset nomic_unsupervised --sampling_strategy cluster_within_domain --exp_name "transductive-1-ds-fake" --num_train_epochs 1.2 --learning_rate 1e-5 --embedder "nomic-ai/nomic-bert-2048" --clustering_model gtr_base --clustering_query_to_doc 1 --automatically_deduplicate_documents 1 --automatically_deduplicate_queries 1 --arch query_independent_dt --dataset_info fake --torch_compile 1 --torch_compile_mode max-autotune-no-cudagraphs --ddp_find_unused_parameters 1 
+ 
+
+# CUDA_VISIBLE_DEVICES=4,5,6,7 torchrun --rdzv-backend=c10d --rdzv-endpoint=localhost:0 --nproc_per_node 4 finetune.py --per_device_train_batch_size 224 --per_device_eval_batch_size 224 --use_wandb 1 --bf16 1 --dataset nomic_unsupervised --sampling_strategy cluster_within_domain --exp_name "transductive-1-ds" --num_train_epochs 1.2 --learning_rate 1e-5 --embedder "nomic-ai/nomic-bert-2048" --clustering_model gtr_base --clustering_query_to_doc 1 --automatically_deduplicate_documents 1 --automatically_deduplicate_queries 1 --arch query_independent_dt --dataset_info batch --ddp_find_unused_parameters 1 --torch_compile 1 --torch_compile_mode max-autotune-no-cudagraphs
+# --torch_compile 1 --torch_compile_mode max-autotune
+
+
+# --ddp_find_unused_parameters 1 
