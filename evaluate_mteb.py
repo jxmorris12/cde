e4658231da307c1367f6a4e9f78836be1c2dd522
Jack Morris <jxmorris12@gmail.com>
2024-04-15 15:39:02 +0000

    fix mteb prefixes

--- evaluate_mteb.py
+++ evaluate_mteb.py
@@ -1,10 +1,14 @@
 import argparse
+import functools
 import os
+
+import torch
 
 from lib.embed import DenseEncoder
 from lib.utils import analyze_utils
 
 from mteb import MTEB
+from mteb.evaluation.evaluators import cos_sim
 
 
 # TODO: Save configs or push to hub. Make this nicer.
@@ -22,11 +26,15 @@ ARGS_STR_DICT = {
     # https://wandb.ai/jack-morris/tti-nomic-5/runs/m44bucij/overview?nw=nwuserjxmorris12
     "unsupervised-hard-batch-1": "--per_device_train_batch_size 2048 --per_device_eval_batch_size 2048 --use_wandb 1 --bf16 1 --dataset nomic_unsupervised --sampling_strategy cluster_within_domain --num_train_epochs 4 --learning_rate 2e-5 --embedder nomic-ai/nomic-bert-2048 --dataset_embedder nomic-ai/nomic-bert-2048 --clustering_model gtr_base --clustering_query_to_doc 1 --automatically_deduplicate_documents 1 --automatically_deduplicate_queries 1 --arch biencoder --dataset_info batch --ddp_find_unused_parameters 0 --torch_compile 0 --eval_rerank_topk 1024 --lr_scheduler_type inverse_sqrt --warmup_steps 5600 --disable_dropout 1 --eval_steps 4000 --max_seq_length 512 --max_batch_size_fits_in_memory 128 --use_gc 1 --logging_steps 20 --train_cluster_size 224 --eval_cluster_size 224 --use_prefix 1 --transductive_corpus_size 1024 --save_steps 400 --logit_scale 50 --max_eval_batches 4 --adam_beta2 0.95 --exp_name biencoder-scratch-2--cluster-longer --resume_from_checkpoint saves/2024-04-11-biencoder-scratch-3--cluster/checkpoint-15800/",
 
+    "biencoder-scratch-01-cluster16k": "--per_device_train_batch_size 2048 --per_device_eval_batch_size 2048 --use_wandb 1 --bf16 1 --dataset nomic_unsupervised --sampling_strategy cluster_within_domain --num_train_epochs 1 --learning_rate 2e-5 --embedder nomic-ai/nomic-bert-2048 --dataset_embedder nomic-ai/nomic-bert-2048 --clustering_model gtr_base --clustering_query_to_doc 1 --automatically_deduplicate_documents 1 --automatically_deduplicate_queries 1 --arch biencoder --dataset_info batch --ddp_find_unused_parameters 0 --torch_compile 0 --eval_rerank_topk 2048 --lr_scheduler_type inverse_sqrt --warmup_steps 5600 --disable_dropout 1 --eval_steps 10000 --max_seq_length 512 --max_batch_size_fits_in_memory 128 --use_gc 1 --logging_steps 20 --train_cluster_size 16384 --eval_cluster_size 224 --use_prefix 1 --transductive_corpus_size 1024 --save_steps 1000 --logit_scale 50 --max_eval_batches 4 --exp_name 2024-04-14-biencoder-scratch-01-cluster16k",
+
 }
 
 MODEL_FOLDER_DICT = {
     "unsupervised-reimpl": "/home/paperspace/tti3/saves/2024-04-12-biencoder-scratch-3--domain/",
     "unsupervised-hard-batch-1": "/home/paperspace/tti3/saves/2024-04-13-biencoder-scratch-2--cluster-longer",
+
+    "biencoder-scratch-01-cluster16k": "/home/sasha/tti3/saves/2024-04-14-biencoder-scratch-01-cluster16k",
 }
 
 assert ARGS_STR_DICT.keys() == MODEL_FOLDER_DICT.keys()
@@ -88,6 +96,16 @@ def parse_args() -> argparse.ArgumentParser:
     )
     return parser.parse_args()
 
+def batched_cos_sim(a: torch.Tensor, b: torch.Tensor, batch_size: int) -> torch.Tensor:
+    cos_sims = []
+    i = 0
+    while i < len(b):
+        cos_sims.append(
+            cos_sim(a, b[i: i+batch_size])
+        )
+        i += batch_size
+    return torch.cat(cos_sims, dim=0)
+
 def main():
     args = parse_args()
     model_folder = MODEL_FOLDER_DICT[args.model_key]
@@ -100,18 +118,21 @@ def main():
         model_name_or_path=trainer.model.config.embedder,
         max_seq_length=trainer.model.config.max_seq_length,
         encoder=trainer.model,
-        query_prefix="search_query",
-        document_prefix="search_document",
+        query_prefix="search_query: ",
+        document_prefix="search_document: ",
     )
 
     for task_idx, task in enumerate(TASK_LIST_RETRIEVAL):
         print(f"Beginning {task} ({task_idx+1} / {len(TASK_LIST_RETRIEVAL)})")
         evaluation = MTEB(tasks=[task], task_langs=["en"])
+        evaluation.score_functions["cos_sim"] = functools.partial(
+            batched_cos_sim, batch_size=50_000
+        )
         evaluation.run(
             mteb_encoder, 
             output_folder=os.path.join("results_mteb", args.model_key),
-            batch_size=1024, 
-            corpus_chunk_size=100_000
+            batch_size=512,# 1024, 
+            corpus_chunk_size=200_000
         )
     
 
