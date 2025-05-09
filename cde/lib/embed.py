eafce4f5f90a599c927b14c824daec6d208751b0
Jack Morris <jxmorris12@users.noreply.github.com>
2025-05-09 16:03:17 -0400

    update dense_encoder

--- cde/lib/embed.py
+++ cde/lib/embed.py
@@ -1,8 +1,7 @@
-from typing import Dict, List, Mapping, Optional, Union
+from typing import Any, Dict, List, Mapping, Optional, Union
 
 import functools
 import os
-import random
 
 import datasets
 import numpy as np
@@ -19,10 +18,6 @@ from cde.lib.tensor import (
 )
 
 
-fingerprint_rng = random.Random()
-def generate_random_fingerprint(nbits: int = 64) -> str:
-    return f"{fingerprint_rng.getrandbits(nbits):0{nbits//4}x}"
-
 def embed_dataloader(
         encoder, 
         data_loader, col: str, 
@@ -30,7 +25,6 @@ def embed_dataloader(
         leave_progress_bar: bool = True, 
         convert_to_tensor: bool = True,
         output_device: str = "cuda",
-        output_dtype: torch.dtype = torch.float32,
         **kwargs
     ) -> List[torch.Tensor]:
     encoded_embeds = []
@@ -45,12 +39,12 @@ def embed_dataloader(
             batch_dict.pop("token_type_ids")
         batch_dict = move_to_cuda(batch_dict)
         # TODO: Intelligently look into encoder kwargs and delete unmatching ones.
-        for unwanted_key in ["prompt_name", "request_qid"]:
+        for unwanted_key in ["prompt_name", "prompt_type", "request_qid", "task_name"]:
             if unwanted_key in kwargs:
                 kwargs.pop(unwanted_key)
         with torch.autocast(embed_device, dtype=torch.bfloat16), torch.no_grad():
             outputs = encoder(**batch_dict, **kwargs)
-        if hasattr(outputs, 'pooler_output') and not (outputs.pooler_output is None):
+        if hasattr(outputs, 'pooler_output') and outputs.pooler_output is not None:
             embeds = outputs.pooler_output
         elif hasattr(outputs, 'last_hidden_state'):
             embeds = mean_pool(
@@ -60,11 +54,10 @@ def embed_dataloader(
         else:
             # already pooled
             embeds = outputs
-        
         encoded_embeds.append(embeds.cpu())
         
     if convert_to_tensor:
-        return torch.cat(encoded_embeds, dim=0).to(output_device).to(output_dtype)
+        return torch.cat(encoded_embeds, dim=0).to(torch.float32).to(output_device)
     else:
         encoded_embeds = [embeds.cpu().float().numpy() for embeds in encoded_embeds]
         output_array = np.concatenate(encoded_embeds, axis=0)
@@ -94,30 +87,11 @@ def move_to_cuda(sample):
     return _move_to_cuda(sample)
 
 
-def tokenize_transform(
-        examples: Dict[str, List],
-        prefix: str,
-        col: str,
-        max_length: int,
-        max_num_chars: int,
-        tokenizer: transformers.PreTrainedTokenizer,
-    ) -> Dict[str, torch.Tensor]:
-    texts = examples[col]
-    batch_dict = tokenizer(
-        [prefix + t[:max_num_chars] for t in texts],
-        max_length=max_length,
-        padding=True,
-        truncation=True,
-        return_tensors="pt",
-    )
-    other_cols = [k for k in examples.keys() if k != col]
-    return {**batch_dict, **{k: examples[k] for k in other_cols}}
-
 class DenseEncoder(torch.nn.Module):
     def __init__(
             self, 
             model_name_or_path: str = "", 
-            max_seq_length = 128, 
+            max_seq_length = 512, 
             encoder: Optional[torch.nn.Module] = None,
             query_prefix: str = "",
             document_prefix: str = "",
@@ -128,21 +102,40 @@ class DenseEncoder(torch.nn.Module):
         self.encoder = encoder
         self.model_name_or_path = model_name_or_path
         self.tokenizer = transformers.AutoTokenizer.from_pretrained(
-            model_name_or_path,
-            padding_side="right",
-        )
-        if not (self.tokenizer.pad_token) and self.tokenizer.bos_token:
-            self.tokenizer.pad_token = self.tokenizer.bos_token
-        self.tokenizer.add_eos_token = True
+            model_name_or_path)
+        # Special loading for autoregressive models. TODO make this more general.
+        if "Llama" in model_name_or_path:
+            self.tokenizer = transformers.AutoTokenizer.from_pretrained(model_name_or_path)
+            self.tokenizer.pad_token = self.tokenizer.eos_token
+            self.tokenizer.add_eos_token = True
+
         self.gpu_count = torch.cuda.device_count()
+
         self.model_is_on_device = False
         self.max_length = max_seq_length
         self.query_prefix = query_prefix
         self.document_prefix = document_prefix
         self.model_kwargs = {}
-        self._max_num_chars = (self.max_length * 16)
+        self._max_num_chars = (self.max_length * 4)
         self.normalize_embeds = normalize_embeds
         self.default_doc_prefix = default_doc_prefix
+
+    def tokenize_transform(
+            self,
+            examples: Dict[str, List],
+            prefix: str,
+            col: str,
+            max_length: int,
+        ) -> Dict[str, torch.Tensor]:
+        texts = examples[col]
+        batch_dict = self.tokenizer(
+            [prefix + t[:self._max_num_chars] for t in texts],
+            max_length=max_length,
+            padding=True,
+            truncation=True,
+        )
+        other_cols = [k for k in examples.keys() if k != col]
+        return {**batch_dict, **{k: examples[k] for k in other_cols}}
 
     def _consider_putting_model_on_device(self) -> None:
         if self.model_is_on_device:
@@ -151,7 +144,7 @@ class DenseEncoder(torch.nn.Module):
             print("[DenseEncoder] initializing from", self.model_name_or_path)
             self.encoder = transformers.AutoModel.from_pretrained(
                 self.model_name_or_path, 
-                # torch_dtype=torch.float16,
+                torch_dtype=torch.float16,
                 trust_remote_code=True,
             )
             self.encoder.eval()
@@ -184,7 +177,6 @@ class DenseEncoder(torch.nn.Module):
             col: str,
             prefix: str,
             batch_size: int,
-            num_workers: Optional[int] = None,
         ) -> torch.utils.data.DataLoader:
         if isinstance(dataset, list):
             if isinstance(dataset[0], str):
@@ -194,16 +186,13 @@ class DenseEncoder(torch.nn.Module):
             else:
                 raise RuntimeError("unknown dataset format to encode")
         
-        print("_create_dataloader() called with batch size:", batch_size, "and num_workers:", num_workers)
         os.environ["TOKENIZERS_PARALLELISM"] = "0"
 
         tokenize_fn = functools.partial(
-            tokenize_transform, 
+            self.tokenize_transform, 
             prefix=prefix,
             col=col, 
             max_length=self.max_length,
-            max_num_chars=self._max_num_chars,
-            tokenizer=self.tokenizer,
         )
         if isinstance(dataset, datasets.IterableDataset):
             dataset = dataset.map(tokenize_fn, batched=True)
@@ -212,19 +201,19 @@ class DenseEncoder(torch.nn.Module):
             dataset.set_transform(tokenize_fn)
         data_collator = transformers.DataCollatorWithPadding(self.tokenizer, pad_to_multiple_of=8)
         effective_batch_size = (batch_size * max(1, self.gpu_count))
-        if num_workers is None:
-            num_workers = min(len(os.sched_getaffinity(0)), self.gpu_count * 4)
+        num_workers = min(len(os.sched_getaffinity(0)), self.gpu_count * 4)
+        num_workers = 0 # TMP
+        print(f"[DenseEncoder] num_workers={num_workers}, len(dataset)={len(dataset)} normalize_embeds={self.normalize_embeds} max_length={self.max_length}")
 
         try:
             len_dataset = len(dataset)
-            effective_batch_size = min(effective_batch_size, len(dataset) // max(1, num_workers))
+            effective_batch_size = min(effective_batch_size, max(1, len(dataset) // max(num_workers, 1)))
         except TypeError:
             # iterable dataset has no len
             len_dataset = "unknown"
             effective_batch_size = batch_size
-        effective_batch_size = max(1, effective_batch_size)
 
-        print(f"[DenseEncoder] created dataloader with {num_workers} workers, effective_batch_size={effective_batch_size}, len(dataset)={len_dataset}")
+        print(f"[DenseEncoder] created dataloader with {num_workers} workers, num_gpus={self.gpu_count}->effective_batch_size={effective_batch_size}, len(dataset)={len_dataset}, max_length={self.max_length}")
         return torch.utils.data.DataLoader(
             dataset,
             batch_size=effective_batch_size,
@@ -245,9 +234,8 @@ class DenseEncoder(torch.nn.Module):
             prefix: str = "", 
             show_progress_bar: bool = True,
             convert_to_tensor: bool = True,
-            output_device: str = "cuda",
-            output_dtype: torch.dtype = torch.float32,
-            num_workers: Optional[int] = None,
+            output_device: str = "cpu",
+            prompt_type: Optional[Any] = None,
             **kwargs,
         ) -> Union[torch.Tensor, np.ndarray]:
         """ Returns a list of embeddings for the given sentences.
@@ -258,15 +246,23 @@ class DenseEncoder(torch.nn.Module):
         Returns:
             `List[np.ndarray]` or `List[tensor]`: List of embeddings for the given sentences
         """
-        if not len(prefix) and self.default_doc_prefix:
+        if prompt_type is not None:
+            # This is required to make our models work with 
+            # MTEB, unfortunately.
+            if prompt_type.title() == "Query":
+                prefix = self.query_prefix
+            elif prompt_type.title() in ["Document", "Passage"]:
+                prefix = self.document_prefix
+            else: 
+                raise ValueError(f"Unknown prompt type: {prompt_type}")
+        elif not len(prefix) and self.default_doc_prefix:
             prefix = self.document_prefix
-        
+
         data_loader = self._create_dataloader(
             dataset=dataset,
             col=col,
             batch_size=batch_size,
             prefix=prefix,
-            num_workers=num_workers,
         )
         self._consider_putting_model_on_device()
 
@@ -276,13 +272,12 @@ class DenseEncoder(torch.nn.Module):
             len_dataset = 0
 
         show_progress_bar = (len_dataset >= 128) and show_progress_bar
-        print(f"[DenseEncoder] encode() calling embed_dataloader (len(dataset)={len_dataset}, convert_to_tensor={convert_to_tensor}, output_device={output_device})")
+        print(f"[DenseEncoder] encode() calling embed_dataloader (len(dataset)={len_dataset}, convert_to_tensor={convert_to_tensor}, output_device={output_device}, prefix={prefix})")
         encoded_embeds = embed_dataloader(
             self.encoder,
             data_loader, 
             col=col,
             convert_to_tensor=convert_to_tensor,
-            output_dtype=output_dtype,
             show_progress_bar=show_progress_bar,
             output_device=output_device,
             **kwargs,
@@ -309,8 +304,6 @@ def embed_with_cache(
         batch_size: int = 4096,
         max_seq_length: int = 512,
         model: Optional[DenseEncoder] = None,
-        normalize_embeds: bool = False,
-        prefix: str = "",
     ) -> datasets.Dataset:
     embedder_cache_path = model_name.replace('/', '__')
     cache_folder = os.path.join(get_cde_cache_dir(), 'corpus_embeddings', embedder_cache_path)
@@ -330,18 +323,8 @@ def embed_with_cache(
 
     print(f"[embed_with_cache] encoding {len(d)} with batch size {batch_size}")
     if model is None:
-        model = DenseEncoder(
-            model_name, 
-            max_seq_length=max_seq_length,
-            normalize_embeds=normalize_embeds,
-        )
-    embeddings = model.encode(
-        d_subset, 
-        col, 
-        batch_size=batch_size, 
-        output_device="cpu",
-        prefix=prefix,
-    )
+        model = DenseEncoder(model_name, max_seq_length=max_seq_length)
+    embeddings = model.encode(d_subset, col, batch_size=batch_size, output_device="cpu")
 
     print(f"[embed_with_cache] creating datasets")
 
